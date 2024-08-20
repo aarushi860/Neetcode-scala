fc2a4ebac3ad5c26a1fe39e4929d4e6f42c1dadc
Aarushi Maheshwari <aarushi860@gmail.com>
2024-08-21 04:16:35 +0530

    Create Leet1898MaximumNumberofRemovableCharacters.scala

---
+++ src/main/scala/binarySearch/medium/Leet1898MaximumNumberofRemovableCharacters.scala
@@ -0,0 +1,19 @@
+package binarySearch.medium
+
+object Leet1898MaximumNumberofRemovableCharacters {
+  def maximumRemovals(s: String, p: String, removable: Array[Int]): Int = {
+    findOPtimal(s, p, removable, 0, removable.length - 1)
+  }
+
+  private def findOPtimal(s: String, p: String, removable: Array[Int], left: Int, right: Int): Int = {
+    if (left > right) right
+    else {
+      val mid = (left + right) / 2
+      val idxToConsider = removable.take(mid)
+      val updatedString = s.zipWithIndex.filter(x => !idxToConsider.contains(x._2))
+      if (updatedString.map(_._1).mkString.contains(p))
+        findOPtimal(s, p, removable, mid, right)
+      else findOPtimal(s, p, removable, left, mid - 1)
+    }
+  }
+}
